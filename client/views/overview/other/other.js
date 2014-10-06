f12adfd7f32aa7994748f0ed6c0549dbd9fbebea
Chaosbohne <chaosbohne@gmail.com>
2014-10-06 12:01:33 +0000

    adding styles

---
+++ client/views/overview/other/other.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* Other: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Other.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Other.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Other: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Other.created = function () {
+};
+
+Template.Other.rendered = function () {
+};
+
+Template.Other.destroyed = function () {
+};
+
+
