dd161b720204b2ef506185947fe916ca361f0894
Chaosbohne <chaosbohne@gmail.com>
2014-10-07 10:07:53 +0000

    adding views

---
+++ client/views/overview/meetup/meetup.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* Meetup: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Meetup.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Meetup.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Meetup: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Meetup.created = function () {
+};
+
+Template.Meetup.rendered = function () {
+};
+
+Template.Meetup.destroyed = function () {
+};
+
+
