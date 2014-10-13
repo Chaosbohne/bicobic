0131a1cde143e9e1d4b98af30281782894dad6fd
Chaosbohne <chaosbohne@gmail.com>
2014-10-13 18:56:26 +0000

    Git experiments

---
+++ server/db/blubb.js
@@ -0,0 +1,15 @@
+if(Meteor.users.find().count() === 0){
+  /*
+  var userId = Accounts.createUser({
+    username: 'name',
+    password: 'password',
+    email: 'bicobic@gmx.de',
+    profile: {
+      name: 'Rico Ruszewski'
+    }
+  });
+  */
+}
+
+
+if(Posts.find().count() === 0) {
