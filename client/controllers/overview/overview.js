b0070b2087cd9b38cd69d397fb7cf72f7fea9f0b
Chaosbohne <chaosbohne@gmail.com>
2014-10-23 14:02:52 +0000

    Fix on page scroll offset

--- client/controllers/overview/overview.js
+++ client/controllers/overview/overview.js
@@ -22,14 +22,18 @@ OverviewController = RouteController.extend({
   },
   
   onAfterAction: function () {
+    // now this is important : Deps.afterFlush ensures that iron-router rendering
+    // process has finished inserting the current route template into DOM so we
+    // can manipulate it via jQuery, if you skip this part the HTML element you
+    // want to scroll to might not yet be present in the DOM (this is probably
+    // why your code fails in the first place)    
     if (this.params.hash) {
-      var scrollTop = $("#" + this.params.hash);
-      if(scrollTop && scrollTop.offset()) {
-        scrollTop = scrollTop.offset().top;
+      Deps.afterFlush(_.bind(function() {
+        var scrollTop = $("#" + this.params.hash).offset().top;
         $("html,body").animate({
           scrollTop: scrollTop
-        });    
-      }
-    }  
+        });
+      }, this));
+    }      
   }
 });
